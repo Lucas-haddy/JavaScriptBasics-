a3dccc1d9c9878689a8f3a07b371b6b8706177e6
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-03-09 10:58:41 +1300

    made an 'else if' statement and a Ternary operator example

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const time = 12;
+const isAfternoon = true
+/*
+if (time >= 12 && isAfternoon) {
+    console.log("Good afternoon!");
+} else if (time >= 12 && !isAfternoon) {
+    console.log("Good morning!");
+}
+*/
+ // Ternary operator example
+const timeOfDay = isAfternoon === true ? 'Afternoon' : 'Morning';
+console.log('Time of Day (Ternary)', timeOfDay);
